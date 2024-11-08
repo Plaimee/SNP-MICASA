bb523ea0867d8749b631af14ad754acd9c381c65
ponpitak <bankppt.srm@gmail.com>
2024-11-08 22:45:11 +0700

    update color variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'red-main': '#ff1123',
+      },
+    },
   },
   plugins: [],
 };
