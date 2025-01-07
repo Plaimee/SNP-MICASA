4ee751fcf8dc56e90da3279c6e8227507e6cb205
ponpitak <ponpitak@ghugcode.co.th>
2025-01-07 18:29:55 +0700

    config => config build command and dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { host: true },
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    server: { host: true },
+    plugins: [react()],
+    define: { 'process.env': JSON.stringify(mode) },
+    resolve: { alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }] },
+  };
 });
